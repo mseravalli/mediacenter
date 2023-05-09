c29480db790b06390bcf7c8d5395daf17a29eddd
Marco Seravalli <seravalli@google.com>
2023-05-09 22:39:04 +0200

    [WIP] Launch the command in an ansync manner

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ use actix_web_lab::respond::Html;
 use log::{debug, info};
 use std::collections::HashMap;
 use std::process::Command;
+use std::sync::Mutex;
 use tera::Tera;
 
 // store tera template in application state
@@ -33,14 +34,14 @@ async fn index(
     Ok(Html(s))
 }
 
-fn create_vpn_server(region: &str) -> Result<String, String> {
-    Command::new("/tmp/test.sh")
-        .output()
-        .expect("failed to execute process");
+fn create_vpn_server(path: &str, region: &str) -> Result<String, String> {
+    Command::new(path)
+        .spawn()
+        .expect("Failed to execute process");
     Ok(format!("{region}"))
 }
 
-async fn connect(req: HttpRequest) -> HttpResponse {
+async fn connect(req: HttpRequest, data: web::Data<AppState>) -> HttpResponse {
     debug!("REQ: {req:?}");
     info!("REQ: {:?}", req.query_string());
     let query = web::Query::<HashMap<String, String>>::from_query(req.query_string())
@@ -52,20 +53,33 @@ async fn connect(req: HttpRequest) -> HttpResponse {
     });
 
     let res = location.and_then(|x| match x.as_str() {
-        l if l == "italy" || l == "germany" => create_vpn_server(l)
+        l if l == "italy" || l == "germany" => {
+            // sdf
+            if data.running_process.lock().unwrap().is_some() {
+                Ok("Already running".to_string())
+            } else {
+                create_vpn_server(&data.vnp_server_setup_path, l)
+            }
+            .map(|x| l.to_string())
             .map_err(|e| {
                 HttpResponse::InternalServerError().json(format!(
                     "Error occurred while setting up the connection: {e}"
                 ))
             })
-            .map(|x| (l.to_string(), x)),
+        }
         l => Err(HttpResponse::BadRequest().json(format!("Cannot connect to {l}"))),
     });
 
     match res {
-        Ok(x) => HttpResponse::Ok().json(format!("Started VPN setup to {}", x.0)),
+        Ok(x) => HttpResponse::Ok().json(format!("Started VPN setup to {}", x)),
         Err(e) => e,
     }
+}
+
+struct AppState {
+    vnp_server_setup_path: String,
+    vpn_client_path: String,
+    running_process: Mutex<Option<usize>>, // <- Mutex is necessary to mutate safely across threads
 }
 
 #[actix_web::main]
@@ -75,10 +89,17 @@ async fn main() -> std::io::Result<()> {
     let port = 8080;
     info!("starting HTTP server at http://localhost:{}", port);
 
-    HttpServer::new(|| {
+    let app_state = web::Data::new(AppState {
+        vnp_server_setup_path: format!(""),
+        vpn_client_path: format!(""),
+        running_process: Mutex::new(None),
+    });
+
+    HttpServer::new(move || {
         let tera = Tera::new(concat!(env!("CARGO_MANIFEST_DIR"), "/templates/**/*")).unwrap();
 
         App::new()
+            .app_data(app_state.clone())
             .app_data(web::Data::new(tera))
             .wrap(middleware::Logger::default())
             .service(Files::new("/img", "static/img/").show_files_listing())
