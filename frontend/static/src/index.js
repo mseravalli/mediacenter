10f40cadb8e3d9118eaf575cc0118630304b8cea
Marco Seravalli <seravalli@google.com>
2023-05-10 17:39:03 +0200

    Simplify js logic to allow to reuse it.

--- frontend/static/src/index.js
+++ frontend/static/src/index.js
@@ -1,81 +1,52 @@
+function create_connect_ajax(country_code) {
+  return function connect() {
+    $.ajax({
+      url: "http://localhost:8080/connect",
+      data: {
+        location: country_code
+      },
+      success: function( result ) {
+        console.log("Connection in progress.");
+      },
+      error: function() {
+        console.log("Connection failed.");
+      }
+    });
+  };
+}
+
+function create_is_connected_ajax(country_code) {
+  return $.ajax({
+    url: "http://localhost:8080/is_connected",
+    data: {
+      location: country_code
+    },
+    success: function( result ) {
+      $("#vpn_"+country_code+"_img").removeClass("green_background");
+      $("#vpn_"+country_code+"_img").removeClass("yellow_background");
+      $("#vpn_"+country_code+"_img").removeClass("red_background");
+      if (result == "Already connected to "+country_code+".") {
+        $("#vpn_"+country_code+"_img").addClass("green_background");
+      } else if (result == "Connection to "+country_code+" in progress.") {
+        $("#vpn_"+country_code+"_img").addClass("yellow_background");
+      } else {
+        $("#vpn_"+country_code+"_img").addClass("red_background");
+      }
+    },
+    error: function() {
+      $("#vpn_"+country_code+"_img").addClass("red_background");
+    }
+  });
+}
+
 $(document).ready(function(){
-  $("#vpn_italy").click(
-    function connect(){
-      $.ajax({
-        url: "http://localhost:8080/connect",
-        data: {
-          location: "IT"
-        },
-        success: function( result ) {
-          console.log("Connection in progress.");
-        },
-        error: function() {
-          console.log("Connection failed.");
-        }
-      });
-    }
-  );
-  $("#vpn_germany").click(
-    function connect(){
-      $.ajax({
-        url: "http://localhost:8080/connect",
-        data: {
-          location: "DE"
-        },
-        success: function( result ) {
-          console.log("Connection in progress.");
-        },
-        error: function() {
-          console.log("Connection failed.");
-        }
-      });
-    }
-  );
+  $("#vpn_IT").click(create_connect_ajax("IT"));
+  $("#vpn_DE").click(create_connect_ajax("DE"));
 });
 
 function poll(){
-  $.ajax({
-    url: "http://localhost:8080/is_connected",
-    data: {
-      location: "IT"
-    },
-    success: function( result ) {
-      $("#vpn_italy_img").removeClass("green_background");
-      $("#vpn_italy_img").removeClass("yellow_background");
-      $("#vpn_italy_img").removeClass("red_background");
-      if (result == "Already connected to IT.") {
-        $("#vpn_italy_img").addClass("green_background");
-      } else if (result == "Connection to IT in progress.") {
-        $("#vpn_italy_img").addClass("yellow_background");
-      } else {
-        $("#vpn_italy_img").addClass("red_background");
-      }
-    },
-    error: function() {
-      $("#vpn_italy_img").addClass("red_background");
-    }
-  });
-  $.ajax({
-    url: "http://localhost:8080/is_connected",
-    data: {
-      location: "DE"
-    },
-    success: function( result ) {
-      $("#vpn_germany_img").removeClass("green_background");
-      $("#vpn_germany_img").removeClass("yellow_background");
-      $("#vpn_germany_img").removeClass("red_background");
-      if (result == "Already connected to DE.") {
-        $("#vpn_germany_img").addClass("green_background");
-      } else if (result == "Connection to DE in progress.") {
-        $("#vpn_germany_img").addClass("yellow_background");
-      } else {
-        $("#vpn_germany_img").addClass("red_background");
-      }
-    },
-    error: function() {
-      $("#vpn_germany_img").addClass("red_background");
-    }
-  });
+  create_is_connected_ajax("IT");
+  create_is_connected_ajax("DE");
 }
 setInterval(poll, 1000);
 
